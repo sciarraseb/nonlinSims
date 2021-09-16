33e0998706fb37ba9da64864564b3da7b9de759b
Seb Sciarra <ssciarra@uoguelph.ca>
2021-09-15 23:52:03 -0400

    Fix error with data.table.

--- R/generate_person_scores.R
+++ R/generate_person_scores.R
@@ -7,6 +7,7 @@
 #' @param measurement_days days of measurement
 #' @param time_period length of time over which measurements are taken
 #' @return Returns a data table.
+#' @import data.table
 #' @export
 generate_group_scores <- function(num_measurements, param_table, measurement_days, time_period) {
   
@@ -25,29 +26,29 @@ generate_group_scores <- function(num_measurements, param_table, measurement_day
   true_scores_ls <- vector(mode = 'list', length = num_people)
   
   for (person in 1:nrow(param_table)) {
-    obs_scores_ls[[person]] <- generate_ind_scores(param_table = param_table[person, ],
+    obs_scores_ls[[person]] <- generate_ind_scores(ind_param_values = param_table[person, ],
                                                    num_measurements, measurement_days)
-    true_scores_ls[[person]] <- generate_pop_scores(param_table = param_table[person, ],
+    true_scores_ls[[person]] <- generate_pop_scores(ind_param_values = param_table[person, ],
                                                     num_measurements, measurement_days)
   }
   
-  #unlist obs_scores_ls
+  #unlist obs_scores_ls and true_scores_ls and append to data table
   empty_data$obs_score <- unlist(obs_scores_ls)
   empty_data$true_score <- unlist(true_scores_ls)
   
   return(empty_data)
 }
 
-generate_ind_scores <- function(param_table, num_measurements, measurement_days, person_ID) {
+generate_ind_scores <- function(ind_param_values, num_measurements, measurement_days, person_ID) {
   
   #extract error values for a person
-  ind_error_values <- extract_ind_error_values(param_table = param_table,
+  ind_error_values <- extract_ind_error_values(ind_param_values = ind_param_values,
                                                num_measurements = num_measurements)
   
   #extract parameter values for a person
-  diff_j <- param_table[['diff_fixed']] + param_table[['diff_random']]
-  beta_j <- param_table[['beta_fixed']] + param_table[['beta_random']]
-  gamma_j <- param_table[['gamma_fixed']] + param_table[['gamma_random']]
+  diff_j <- ind_param_values[['diff_fixed']] + ind_param_values[['diff_random']]
+  beta_j <- ind_param_values[['beta_fixed']] + ind_param_values[['beta_random']]
+  gamma_j <- ind_param_values[['gamma_fixed']] + ind_param_values[['gamma_random']]
   
   #compute scores at each time point for one person
   ind_scores <- diff_j/(1 + exp((beta_j - measurement_days)/gamma_j)) + ind_error_values
@@ -55,21 +56,21 @@ generate_ind_scores <- function(param_table, num_measurements, measurement_days,
   return(ind_scores)
 }
 
-extract_ind_error_values <- function(param_table, num_measurements){
+extract_ind_error_values <- function(ind_param_values, num_measurements){
   
   #extract name of error columns and use them to return vector of error values
-  error_cols <- tail(names(param_table), num_measurements)
-  ind_error_values <- as.vector(t(param_table[ , ..error_cols]))
+  error_cols <- tail(names(ind_param_values), num_measurements)
+  ind_error_values <- as.vector(t(ind_param_values[ , ..error_cols]))
   
   return(ind_error_values)
 }
 
-generate_pop_scores <- function(param_table, num_measurements, measurement_days) {
+generate_pop_scores <- function(ind_param_values, num_measurements, measurement_days) {
   
   #extract parameter values for a person
-  diff_j <- param_table[['diff_fixed']] + param_table[['diff_random']]
-  beta_j <- param_table[['beta_fixed']] + param_table[['beta_random']]
-  gamma_j <- param_table[['gamma_fixed']] + param_table[['gamma_random']]
+  diff_j <- ind_param_values[['diff_fixed']] + ind_param_values[['diff_random']]
+  beta_j <- ind_param_values[['beta_fixed']] + ind_param_values[['beta_random']]
+  gamma_j <- ind_param_values[['gamma_fixed']] + ind_param_values[['gamma_random']]
   
   #compute scores at each time point for one person
   pop_scores <- diff_j/(1 + exp((beta_j - measurement_days)/gamma_j))
